00edd014bc1cd0b23fee43810c8ccbdc6a099263
tenkai <skylien0966065599@gmail.com>
2021-10-07 19:06:05 +0800

    add add.folder

---
+++ homework/se/lodash/add/add.js
@@ -0,0 +1,6 @@
+// _.add(6, 4) => 10
+
+export function add(n1, n2){
+    var sum = n1 + n2
+    return sum
+}
---
+++ homework/se/lodash/add/example/ex1.js
@@ -0,0 +1,4 @@
+import * as _ from '../add.js'
+
+console.log("_.add(4, 6)=", _.add(4, 6))
+console.log("_.add(6, 4)=", _.add(6, 4))
